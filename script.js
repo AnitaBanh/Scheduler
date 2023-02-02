590adf1853209f91f45462304cf1496fa1b97c44
AB <anitabanhtx@gmail.com>
2023-02-01 22:12:03 -0600

    added class determination and local storage

--- script.js
+++ script.js
@@ -2,9 +2,27 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-// $(window).load(function() {
+$(window).on("load", function() {
+  currentTime();
 
-// }
+  for(let i=0; i < 24; i++) {
+  $("#hour-" + i).children("textarea").text(localStorage.getItem("hour-" + i));
+  }
+
+  for(let i=0; i < 24; i++) {
+    var id = "hour-" + i;
+    var nowHour = now.format("HH");
+    if (i == nowHour) {
+      $("#"+ id).removeClass("future past").addClass("present");
+     }
+    if (i > nowHour) {
+      $("#"+ id).removeClass("past present").addClass("future");
+    }
+    if (i < nowHour) {
+      $("#"+ id).removeClass("future present").addClass("past");
+    }
+   }
+});
 
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
@@ -13,48 +31,72 @@
 // time-block containing the button that was clicked? How might the id be
 // useful when saving the description in local storage?
 let now = dayjs();
-var handleSaveBtn = function (event) {
-  event.preventDefault();
-  var userEntry = $(".col-8").val();
-  $(".saveBtn").click(function () {$('#saveBtn').html();
-  localStorage.setItem('task',value);
-  $('#saveBtn').html(localStorage.content);
-  });
-};
+// var handleSaveBtn = function (event) {
+  // event.preventDefault();
+  // var userEntry = $(".time-block").val();
+  // $(".saveBtn").click(function () {
+    // $('.description').html();
+    // console.log($(this).parent().attr('id'));
+  // localStorage.setItem('task',this.value);
+  // $('#saveBtn').html(localStorage.content);
+//   });
+// };
+
+
+$(".saveBtn").click(function () {
+  console.log();
+  var hour = $(this).parent().attr('id');
+  var contents = $(this).siblings("textarea").val();
+  localStorage.setItem(hour, contents);
+});
+
+
 
 // TODO: Add code to apply the past, present, or future class to each time
 // block by comparing the id to the current hour. HINTS: How can the id
 // attribute of each time-block be used to conditionally add or remove the
 // past, present, and future classes? How can Day.js be used to get the
 // current hour in 24-hour time?
+ //("div[id^='hour-']")
 
-function presentClass () {
-  if(now.format("HH") == (this.id)) {
-  $(".time-block").removeClass("future past").addClass("present");
-} return
-  };
+//  for(let i=0; i<24; i++) {
+//   var id = "hour-" + i;
+//   var nowHour = now.format("HH");
+//   if (i = now) {
+//     $("#"+ id).removeClass("future past").addClass("present");
+//    }
+//   if (i > now) {
+//     $("#"+ id).removeClass("past present").addClass("future");
+//   }
+//   if (i < now) {
+//     $("#"+ id).removeClass("future present").addClass("past");
+//   }
+//  }
 
-// var timeBlock =   $('.col-2')
+// function presentClass () {
+//   if(now.format("HH") == (this.id)) {
+//   $(".time-block").removeClass("future past").addClass("present");
+// } return
+//   };
+
+// var timeBlock =   $('.time-block')
 // function colorScheme () {
 //   var elements = document.querySelector('.time-block');
 //   for (var i = 0; i< elements.length ; i++) {
-//   if timeBlock[i].isBefore(dayjs({})) {
+//   if (timeBlock[i].isBefore(dayjs({}))) {
 //   addClass('.past');
 //   timeBlock.removeClass('.future');
 //   timeBlock.removeClass('.present');
-//   css.background-color:grey
 // }
-// else if {
-//   timeBlock.isAfter(dayjs({})) {
-
+// else 
+//   (timeBlock.isAfter(dayjs({}))) {
 //     timeBlock.removeClass('.present');
 //     timeBlock.removeClass('.past');
-
-//   }
-// else {
+//   } else {
 //   timeBlock.addClass('.present');
 // }}
 // }
+
 
 // TODO: Add code to get any user input that was saved in localStorage and set
 // the values of the corresponding textarea elements. HINT: How can the id
@@ -65,10 +107,10 @@ function presentClass () {
 
 
 function currentTime() {
-  var addDate = $("#currentDay").text(now.format('MMM DD, YYYY'));
+  var addDate = $("#currentDay").text(now.format('dddd, MMM DD, YYYY'));
   setInterval(function () {
     addDate
   }, 1000);
 }
 
-currentTime();
+
